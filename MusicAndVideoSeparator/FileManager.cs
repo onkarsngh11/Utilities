b188207fac64d92861c1bd9873483880f8d20f4c
onkar <onkar@DESKTOP-AVQKCQC>
2023-02-03 13:39:34 +0530

    added more functionality

---
+++ MusicAndVideoSeparator/FileManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MusicAndVideoSeparator
+{
+    public class FileManager : FIleManagerBase
+    {
+        private string[] videoFormats = { "mp4", "3gp", "AVI", "VOB", "mkv", "MP4", "MOV" };
+        private string[] imageFormats = { "jpg", "JPG", "jpeg", "png" };
+        public FileManager(string workingDirectory)
+        {
+            DirectoriesToBeProcessed.Add(workingDirectory);
+            GetAllDirectoriesWithHeirarchy(workingDirectory);
+        }
+
+        public void MoveVideos()
+        {
+            foreach (string directory in DirectoriesToBeProcessed)
+            {
+                foreach (string file in Directory.GetFiles(directory))
+                {
+                    var extension = file.Split(".").Last();
+
+                    //PopulateExtensionsList(extension);
+
+                    //RenameVideosToMp4(file, directory, extension);
+                    //RenameImagesToJpg(file, directory, extension);
+
+                    if (file.EndsWith(".mp4") || file.EndsWith(".3gp"))
+                    {
+                        //MoveVideos(file, videoDestinationPrefix);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(",", ExtensionsList));
+        }
+
+        public void FixJpgFileNameToMp4()
+        {
+            Dictionary<string, float> fileNameWithSize = new Dictionary<string, float>();
+            foreach (string directory in DirectoriesToBeProcessed)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(directory);
+                FileInfo[] files = directoryInfo.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    var size = file.Length / (1024 * 1024);
+
+                    fileNameWithSize.Add(file.FullName, size);
+                }
+            }
+
+            var sortedFiles = fileNameWithSize.OrderByDescending(f => f.Value);
+
+            foreach (var item in sortedFiles)
+            {
+                if ((item.Key.ToLower().Contains("vid") || item.Key.ToLower().Contains("mvi")) && item.Key.Split(".").Last() != "mp4"
+                    && !item.Key.ToLower().Contains("mvimg")) // && (item.Value > 16)
+                {
+                    var split = item.Key.Split(".");
+                    var formFilePath = string.Join(".", split.Take(split.Count() - 1)) + ".mp4";
+
+                    File.Move(item.Key, formFilePath);
+                }
+                Console.WriteLine("{0}: {1}", item.Key, item.Value);
+            }
+        }
+
+        public void MoveVideos(string item, string videoDestinationPrefix)
+        {
+            var items = item.Split("/");
+            var formpath = string.Join('/', items, 2, items.Length - 3);
+
+            Directory.CreateDirectory(videoDestinationPrefix + formpath);
+            File.Move(item, videoDestinationPrefix + item.Substring(10, item.Length - 10));
+        }
+
+        public void RenameVideosToMp4(string file, string directory, string extension)
+        {
+            if (videoFormats.Contains(extension) && extension != "mp4")
+            {
+                var split = file.Split(".");
+                var formFilePath = string.Join(".", split.Take(split.Count() - 1)) + ".mp4";
+
+                File.Move(file, formFilePath);
+            }
+        }
+
+        public void RenameImagesToJpg(string file, string directory, string extension)
+        {
+            if (imageFormats.Contains(extension) && extension != "jpg")
+            {
+                var split = file.Split(".");
+                var formFilePath = string.Join(".", split.Take(split.Count() - 1)) + ".jpg";
+
+                File.Move(file, formFilePath);
+            }
+        }
+
+        internal void RenameImagesToMp4()
+        {
+            foreach (string directory in DirectoriesToBeProcessed)
+            {
+                foreach (string file in Directory.GetFiles(directory))
+                {
+                    var split = file.Split(".");
+                    var formFilePath = string.Join(".", split.Take(split.Count() - 1)) + ".mp4";
+
+                    File.Move(file, formFilePath);
+                }
+            }
+        }
+    }
+}
--- MusicAndVideoSeparator/Program.cs
+++ MusicAndVideoSeparator/Program.cs
@@ -1,44 +1,20 @@
-﻿using System.IO;
+﻿using System.Drawing;
+using System;
+using System.IO;
 
 namespace MusicAndVideoSeparator
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            string videoDestinationPrefix = "D:/Videos/";
-            string[] folders = Directory.GetDirectories("D:/images/");
-            foreach (string lvlonefolder in folders)
-            {
-                string[] lvltwofolders = Directory.GetDirectories(lvlonefolder + "/");
-                string[] lvlonefiles = Directory.GetFiles(lvlonefolder + "/");
-                if (lvltwofolders.Length != 0)
-                {
-                    foreach (string lvltwofolder in lvltwofolders)
-                    {
-                        string[] lvltwofiles = Directory.GetFiles(lvltwofolder + "/");
-                        foreach (var lvltwofile in lvltwofiles)
-                        {
-                            MoveVideos(lvltwofile, videoDestinationPrefix);
-                        }
-                    }
-                }
-                foreach (string lvlonefile in lvlonefiles)
-                {
-                    MoveVideos(lvlonefile, videoDestinationPrefix);
-                }
-            }
-        }
-        public static void MoveVideos(string item, string videoDestinationPrefix)
-        {
-            if (item.EndsWith(".mp4") || item.EndsWith(".3gp"))
-            {
-                var items = item.Split("/");
-                var formpath = string.Join('/', items, 2, items.Length - 3);
+            string WorkingDirectory = @"D:\Memories\Images\1 Onkar wed\3 Sagan\mp4";
+            FileManager videosManager = new FileManager(WorkingDirectory);
+            //ImagesManager imagesManager = new ImagesManager();
 
-                Directory.CreateDirectory(videoDestinationPrefix + formpath);    //videoDestinationPrefix + item.Substring(10, item.Length - 10))
-                File.Move(item, videoDestinationPrefix + item.Substring(10, item.Length - 10));
-            }
+            //videosManager.MoveVideos();
+            //videosManager.FixJpgFileNameToMp4BasedOnSize();
+            videosManager.RenameImagesToMp4();
         }
     }
 }
